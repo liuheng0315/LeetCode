e6e1dd8c19307d4514a4eb289f015d8068cbc185
liuheng <liuheng0315@gmail.com>
2018-04-26 14:20:04 +0800

    最长回文字符串

--- LeetCode05_LongestPalindrome.java
+++ LeetCode05_LongestPalindrome.java
@@ -32,4 +32,24 @@ public class LeetCode05_LongestPalindrome {
             res=cur;
         }
     }
+    //递归的方法
+    public String longestPalindrome_dp(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        int max=0;
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <=j ; i++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1]);
+                if (dp[i][j]) {
+                    if (j - i + 1 > max) {
+                        max=j-i+1;
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
